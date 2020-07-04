7a0509a45559ede275c5a19432e706063dcc7d2b
Misa-Kanuma <misa.kanuma@gmail.com>
2020-07-05 01:28:46 +0900

    [Update]validation, users_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
-    @book = Book.new
+     @book = Book.find(params[:id])
+    @book_comment = BookComment.new
   end
 
   def index
