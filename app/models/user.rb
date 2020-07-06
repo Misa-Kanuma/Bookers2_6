781f94c2f4619307f8de19159472175494844daf
Misa-Kanuma <misa.kanuma@gmail.com>
2020-07-06 15:51:36 +0900

    [Add]フォローする・外す

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :following, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following_user, through: :following, source: :followed
-  has_many :followed_user, through: :followed, source: :follower
+  has_many :followed_user, through: :followed, source: :followering
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
@@ -17,11 +17,11 @@ class User < ApplicationRecord
   validates :introduction, length: {maximum: 50}
 
   def follow(user_id)
-    follower.create(followed_id: user_id)
+    following.create(followed_id: user_id)
   end
 
   def unfollow(user_id)
-    follower.find_by(followed_id:user_id).destroy
+    following.find_by(followed_id:user_id).destroy
   end
 
   def following?(user)
